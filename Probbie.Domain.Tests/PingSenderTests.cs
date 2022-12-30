4e507aa7d8282583af6a5fd024ac0ac26349a819
Patryk Sójkowski <p.sojkowski@krd.pl>
2022-12-30 17:53:06 +0100

    Added ping sender

---
+++ Probbie.Domain.Tests/PingSenderTests.cs
@@ -0,0 +1,47 @@
+﻿using Probbie.Domain.ValueTypes;
+
+namespace Probbie.Domain.Tests
+{
+  [TestFixture]
+  internal class PingSenderTests
+  {
+    [Test]
+    public void Send_SentToLocalhost_ReturnsLatency()
+    {
+      // Arrange
+      var sut = new PingSender();
+
+      // Act
+      var latency = sut.Send("localhost");
+
+      // Assert
+      Assert.That(latency.Value, Is.LessThan(10));
+    }
+
+    [Test]
+    public void Send_SentGoogleDns_ReturnsLatency()
+    {
+      // Arrange
+      var sut = new PingSender();
+
+      // Act
+      var latency = sut.Send("8.8.8.8");
+
+      // Assert
+      Assert.That(latency.Value, Is.LessThan(Latency.Infinity.Value));
+    }
+
+    [Test]
+    public void Send_SentToUnreachableAddress_ReturnsInfinity()
+    {
+      // Arrange
+      var sut = new PingSender();
+
+      // Act
+      var latency = sut.Send("1.2.3.4");
+
+      // Assert
+      Assert.That(latency, Is.EqualTo(Latency.Infinity));
+    }
+  }
+}
